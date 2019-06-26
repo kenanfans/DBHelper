5f07668686f7c8bd25c3d42a686e2ddaded50ae4
KK <KK@windows10.microdone.cn>
2019-06-26 16:55:42 +0800

    Signed-off-by: KK <KK@windows10.microdone.cn>

--- KDO/src/kk/DB/DBHelper.java
+++ KDO/src/kk/DB/DBHelper.java
@@ -1,5 +1,25 @@
 package kk.DB;
 
-public class DBHelper {
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Properties;
 
+
+public class DBHelper {
+	public static ArrayList<DBConn> _DBConnList = new ArrayList<DBConn>();
+	public static Boolean _Inited = false; 
+	
+	public static DBConn GetDefaultConn() throws Exception
+	{
+		if(!_Inited) {
+			throw new Exception("数据库连接尚未初始化");
+		}
+		return _DBConnList.get(0);
+	}
+	
+	public static void InitDBHelper(String _basePath) {
+		
+	}
+	
+	
 }
